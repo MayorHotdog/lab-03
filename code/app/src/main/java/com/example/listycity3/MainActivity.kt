2c589626d1fbca07d83f736b9826424818c80f15
Karl Markowski <karl.markowski05@gmail.com>
2026-09-18 04:54:58 -0600

    did the lab and removed uneeded imports

--- code/app/src/main/java/com/example/listycity3/MainActivity.kt
+++ code/app/src/main/java/com/example/listycity3/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : ComponentActivity() {
                     CityListScreen(
                         cities = cityRepository.cities,
                         onAddCity = { cityRepository.addCity(it) },
-                        onDeleteCity = { cityRepository.deleteCity(it) },
                         onUpdateCity = { oldCity, newCity -> cityRepository.updateCity(oldCity, newCity) },
                         modifier = Modifier.padding(innerPadding)
                     )
